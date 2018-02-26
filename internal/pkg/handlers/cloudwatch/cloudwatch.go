819636c513c1d6ef18fd009ebecb041ad319d14b
Josiah Niedrauer <jniedrauer@gmail.com>
2018-02-25 18:23:26 -0600

    Implement CloudwatchLogsEvent

--- internal/pkg/handlers/cloudwatch/cloudwatch.go
+++ internal/pkg/handlers/cloudwatch/cloudwatch.go
@@ -3,26 +3,22 @@ package cloudwatch
 import (
 	"fmt"
 
-	"github.com/ethereum/go-ethereum/log"
+	"github.com/aws/aws-lambda-go/events"
+	log "github.com/sirupsen/logrus"
 )
-
-type Event struct {
-	ID    float64 `json:"id"`
-	Value string  `json:"value"`
-}
 
 type Response struct {
 	Message string `json:"message"`
 	Ok      bool   `json:"ok"`
 }
 
-func Handler(event Event) (Response, error) {
-	log.Debug("Got event: %s", event.Value)
+func Handler(event events.CloudwatchLogsEvent) (Response, error) {
+	log.Debug("Got event: %s", event.AWSLogs)
 
 	// TODO: Call to another function for actual logic
 
 	return Response{
-		Message: fmt.Sprintf("Processed request ID %f", event.ID),
+		Message: fmt.Sprintf("did a thing"), // TODO: Do something here
 		Ok:      true,
 	}, nil
 }
