31f10ea844dbab61d9b853d84c43882794eaf16c
Josiah Niedrauer <jniedrauer@gmail.com>
2018-03-03 10:58:42 -0600

    Comment out unimplemented app

--- internal/app/elb/main.go
+++ internal/app/elb/main.go
@@ -17,14 +17,9 @@ Optional environment variables:
 */
 package main
 
-import (
-	"github.com/jniedrauer/logs-to-elastic/internal/pkg/handlers/cloudwatch"
-	"github.com/jniedrauer/logs-to-elastic/internal/pkg/logging"
-
-	"github.com/aws/aws-lambda-go/lambda"
-)
-
 func main() {
-	logging.Init()
-	lambda.Start(cloudwatch.Handler)
+	/*
+		logging.Init()
+		lambda.Start(cloudwatch.Handler)
+	*/
 }
